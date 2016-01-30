a2dc403d7c14a398586b95180f1c776c3a69e75e
Erinda <erinda.jaupaj@u-hopper.com>
2016-01-31 00:50:17 +0100

    added rest call for random quotes

---
+++ src/com/unitn/adapter_service/services/Quotes.java
@@ -0,0 +1,15 @@
+package com.unitn.adapter_service.services;
+
+import lombok.Data;
+
+/**
+ * Created by erinda on 1/31/16.
+ */
+
+@Data
+public class Quotes {
+    String quote;
+    String author;
+    String category;
+
+}
---
+++ src/com/unitn/adapter_service/services/QuotesService.java
@@ -0,0 +1,42 @@
+package com.unitn.adapter_service.services;
+
+import retrofit2.Call;
+import retrofit2.GsonConverterFactory;
+import retrofit2.Retrofit;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+
+/**
+ * Created by erinda on 1/31/16.
+ */
+public interface QuotesService {
+
+
+    @Headers({
+            "X-Mashape-Key : FuPi3q56U4mshaZJjiE6ucUEa7TAp1ki9PijsnX0RiklcDlJs8",
+            "Content-Type : application/json",
+            "Accept : application/json"
+    })
+    @POST("/?cat=movies")
+    Call<Quotes> getQuoteFromMovies();
+
+    @Headers({
+            "X-Mashape-Key : FuPi3q56U4mshaZJjiE6ucUEa7TAp1ki9PijsnX0RiklcDlJs8",
+            "Content-Type : application/json",
+            "Accept : application/json"
+    })
+    @POST("/?cat=famous")
+    Call<Quotes> getQuoteFromFamous();
+
+    static QuotesService getService(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://andruxnet-random-famous-quotes.p.mashape.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        return retrofit.create(QuotesService.class);
+    }
+
+
+}
